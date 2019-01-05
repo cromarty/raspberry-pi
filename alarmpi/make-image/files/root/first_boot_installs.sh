a404ae27f1a157e06bc8278c60b6d0aa4dc2fa32
Mike Ray <mike.ray@btinternet.com>
2019-01-05 19:01:27 +0000

    new file:   alarmpi/make-image/files/boot/config.txt.raspbian 	new file:   alarmpi/make-image/files/etc/motd 	modified:   alarmpi/make-image/files/root/first_boot_installs.sh 	modified:   alarmpi/make-image/files/root/second_boot_installs.sh 	modified:   alarmpi/make-image/make-alarmpi-image

--- alarmpi/make-image/files/root/first_boot_installs.sh
+++ alarmpi/make-image/files/root/first_boot_installs.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
+#
+# This script needs to be run the first time a new Arch Linux image is booted.
+#
+# First pacman-key is called to both initiate and
+# populate the keyrings.
+#
+# Then pacman -Syu is called for full synch and upgrade.
+#
+# Before the auto-expansion can run, libnewt and parted need to be
+# installed.
+#
+# At the bottom of this script the call to the auto-expansion
+# script is appended to /boot/cmdline.txt
+#
 
+# Check we are root
 (( EUID == 0 )) || { echo "Script must be run as root" ; exit 1 ; }
 
 set -e
 
-
+# Initiate and populate keyrings
 pacman-key --init
 pacman-key --populate
 
+# synch and upgrade
+pacman -Syu --noconfirm --noprogress
+
+# Install the auto-expansion script dependencies
 pacman -S --noconfirm --noprogressbar --needed libnewt parted
 
 # set up for auto-expansion on next boot
--- alarmpi/make-image/files/root/second_boot_installs.sh
+++ alarmpi/make-image/files/root/second_boot_installs.sh
@@ -3,8 +3,7 @@
 (( EUID == 0 )) || { echo "Script must be run as root" ; exit 1 ; }
 
 pacman -S --noconfirm --noprogressbar --needed \
-    sudo python dmidecode
-
+    sudo python dmidecode dosfstools rsync
 
 sed -i 's|#\( %wheel ALL=(ALL) ALL\)|\1|' /etc/sudoers
 
